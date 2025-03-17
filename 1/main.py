d0fabd0e5b3756f99f48c4f831fcd70e6227e503
nam9nine <southgyuios19@gmail.com>
2025-03-17 13:28:18 +0900

    [modify] 문제 1

--- 1/main.py
+++ 1/main.py
@@ -2,11 +2,10 @@ print("‘Hello Mars")
 try:
     with open("./mission_computer_main.log", "r") as read_f:
         read_lines = read_f.readlines()
-    with open("log_analysis.md", "w", encoding="utf-8") as write_f:
         for line in read_lines:
             line = line.strip()
             print(line)
-            write_f.writelines(line + "\n")
+
 except FileNotFoundError as e:
     print(f"파일을 찾을 수 없습니다: {e}")
 except IOError as e:
