07c5d190941c4df8e3ba551584b079b649ffa3da
nam9nine <southgyuios19@gmail.com>
2025-03-30 16:41:43 +0900

    문제3 인화 물질을 찾아라

---
+++ 3/main.py
@@ -0,0 +1,19 @@
+try:
+    with open("./Mars_Base_Inventory_List.csv", "r") as f:
+
+       list_l = f.readlines()
+       list_l = sorted(list_l, key = lambda x : x.split(",")[4], reverse=True)
+
+       list_e = []
+       list_e.append(list_l[0])
+       for li in list_l[1:]:
+           if float(li.split(",")[4]) >= 0.7:
+               print(li)
+               list_e.append(li)
+    print(list_e)
+   
+    with open("./Mars_Base_Inventory_danger.csv", "w") as w:
+        for i in list_e:
+            w.write(i)
+except FileNotFoundError:
+    print("파일을 찾을 수 없습니다.")
